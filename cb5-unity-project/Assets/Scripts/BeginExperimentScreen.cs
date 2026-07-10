e5827079e37969d6c56e103b4575cd58fc014f5b
eberhaan <antonia.eberharter@umit-tirol.at>
2026-07-10 14:44:41 +0200

    Add files via upload

---
+++ cb5-unity-project/Assets/Scripts/BeginExperimentScreen.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System;
+
+public class BeginExperimentScreen : MonoBehaviour {
+	/*
+	public tk2dUITextInput txtParticipantID;
+
+	public Transform ConditionsPanel;
+	public tk2dUIDropDownMenu ddlMode;
+    
+    // Use this for initialization
+	void Start () {
+		txtParticipantID.Text = Game.ParticipantID + string.Empty;
+
+		if (Game.ConditionCount == 0 || Game.IsRandomAssignment) {
+			ConditionsPanel.gameObject.SetActive(false);		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnBackClicked()
+	{
+		Application.LoadLevel ("ControlPanel");
+	}
+
+	void OnSubmitClicked()
+	{
+		//Game.CurrentCondition =int.Parse( ddlMode.SelectedItem.Substring(ddlMode.SelectedItem.Length- 1));
+		//Game.ParticipantID = txtParticipantID.Text;
+
+		Game.PrefsToFile ();
+		Application.Quit ();
+	}
+	*/
+}
---
+++ cb5-unity-project/Assets/Scripts/ChatContainer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatContainer : MonoBehaviour {
+
+	public Vector3 finalScale;
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
