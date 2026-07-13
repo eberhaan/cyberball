f6fb362d81898b4daee78f4f616ca0774a2a80ff
eberhaan <antonia.eberharter@umit-tirol.at>
2026-07-13 10:57:46 +0200

    Add URL parameter handling for participant ID and condition

--- web-version/config.js
+++ web-version/config.js
@@ -1,1 +1,18 @@
+const urlParams = new URLSearchParams(window.location.search);
 
+
+const participantID =
+    urlParams.get("pid") || "TEST";
+
+
+const condition =
+    urlParams.get("condition") || "1";
+
+
+
+console.log(
+    "Participant:",
+    participantID,
+    "Condition:",
+    condition
+);
